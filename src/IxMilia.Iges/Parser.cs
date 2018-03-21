063820a795014a6cf50ef57478e8b1e3163c245d
Luc Bologna <lbl@p3-group.com>
2018-03-21 14:51:44 +0100

    Delete comment

--- src/IxMilia.Iges/Parser.cs
+++ src/IxMilia.Iges/Parser.cs
@@ -8,34 +8,6 @@ namespace IxMilia.Iges
     internal static class Parser
     {
         private static CultureInfo culture = CultureInfo.GetCultureInfo("en-US");
-        /*
-        public static int ToInt(this string value)
-        {
-            int result = 0;
-            if (int.TryParse(value, out result))
-                return result;
-            else
-                throw new FormatException("'" + value + "': Bad format of number");
-        }
-
-        public static uint ToUInt(this string value)
-        {
-            uint result = 0;
-            if (uint.TryParse(value, out result))
-                return result;
-            else
-                throw new FormatException("'" + value + "': Bad format of number");
-        }
-
-        public static double ToDouble(this string value)
-        {
-            double result = 0;
-            if (double.TryParse(value, out result))
-                return result;
-            else
-                throw new FormatException("'" + value + "': Bad format of number");
-        }
-        */
 
         public static int ToInt(this string value)
         {
